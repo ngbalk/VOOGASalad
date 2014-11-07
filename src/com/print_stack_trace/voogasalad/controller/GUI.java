8ec81670ea71a9fd39f645aad151f7aef62a99ff
Pranava Raparla <pranava.raparla@gmail.com>
2014-11-06 22:22:29 -0500

    Added tab pane to GUI class initialize method

--- src/com/print_stack_trace/voogasalad/controller/GUI.java
+++ src/com/print_stack_trace/voogasalad/controller/GUI.java
@@ -1,13 +1,20 @@
+/**
+ * @author Pranava Raparla
+ * @author Zachary Podbella
+ * Date Created: 11/06/14
+ * Date Modified: 11/06/14
+ */
 package com.print_stack_trace.voogasalad.controller;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class GUI {
 	public static Scene init(double width, double height) {
 		Group root = new Group();
-		//TODO: @pdiddy Add Tab Pane to root, and then tabs
+		root.getChildren().add(new TabPane());
 		Scene myScene = new Scene(root, width, height);
 		return myScene;
 	}
