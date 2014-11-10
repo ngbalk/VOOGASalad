0ba857051f70f5a4dbb571de6b1fa775ac103a3c
Ethan Chang <ejc24@duke.edu>
2014-11-09 20:21:09 -0500

    add skeleton code

---
+++ src/com/print_stack_trace/voogasalad/controller/author/Goal.java
@@ -0,0 +1,8 @@
+package com.print_stack_trace.voogasalad.controller.author;
+
+public abstract class Goal {
+    
+    public Goal() {
+        
+    }
+}
---
+++ src/com/print_stack_trace/voogasalad/controller/author/Obstacle.java
@@ -0,0 +1,16 @@
+package com.print_stack_trace.voogasalad.controller.author;
+
+public class Obstacle extends Block{
+
+    public Obstacle (String id) {
+        super(id);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update () {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
