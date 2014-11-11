d149541ac1afcd1d9abed832e5587d881280269f
phswrestler125 <mac86@duke.edu>
2014-11-10 23:00:18 -0500

    Adding HighScores, and Score Class

---
+++ src/com/print_stack_trace/voogasalad/player/HighScores.java
@@ -0,0 +1,30 @@
+package com.print_stack_trace.voogasalad.player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HighScores {
+	/***
+	 * Class used to Store HighScores for the game
+	 */
+	private double highestScore;
+	//private double currentScore;
+	private Map<String,Double> scores; //change String to Player Object (if created)
+	public HighScores(){
+		scores = new HashMap<String,Double>();
+	}
+	
+	public void resetHighScores(){
+		scores.clear();
+	}
+	
+	public void updateScore(Player p, double score){
+		//check is player score already exist
+		//check if current player score is greater than high score
+		//add/edit high score
+	
+	public String toString() {
+		//iterate over map, print HighScores
+	}
+	
+}
