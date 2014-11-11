47c3cad7f1b181468b3101f29060e434749adf0a
Ashwin Kommajesula <ashwin.kommajesula@gmail.com>
2014-11-10 21:49:14 -0500

    data public methods

--- src/com/print_stack_trace/voogasalad/model/data/GameData.java
+++ src/com/print_stack_trace/voogasalad/model/data/GameData.java
@@ -1,5 +1,41 @@
 package com.print_stack_trace.voogasalad.model.data;
 
-public class GameData {
+import java.lang.reflect.Type;
 
+import com.print_stack_trace.voogasalad.model.LevelModel;
+
+public class GameData {
+	public GameData() {
+
+	}
+
+	/**
+	 * Will take in a level written by authoring environment as a parameter,
+	 * write it to a JSON file
+	 * 
+	 * @param lvl
+	 */
+	public void writeLevel(LevelModel lvl) {
+
+	}
+
+	/**
+	 * GamePlayer pushing gameinfo
+	 */
+	public void writeGameData() {
+
+	}
+
+	/**
+	 * Will convert a src object, with its type paramter, into its equivalent
+	 * JSON representation to be saved
+	 * 
+	 * @param src
+	 * @param typeOfSrc
+	 * @return
+	 */
+	public String toJson(Object src, Type typeOfSrc) {
+		return null;
+
+	}
 }
