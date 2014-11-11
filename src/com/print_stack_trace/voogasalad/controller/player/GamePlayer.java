6a5f772be0834dc7145ba20900ed87d3de887859
phswrestler125 <mac86@duke.edu>
2014-11-10 23:32:15 -0500

    Added ButtonFramework for pasuing/resuming/stoping/saving game, created method for all eventhandler for those buttons

--- src/com/print_stack_trace/voogasalad/controller/player/GamePlayer.java
+++ src/com/print_stack_trace/voogasalad/controller/player/GamePlayer.java
@@ -1,17 +1,29 @@
 package com.print_stack_trace.voogasalad.controller.player;
 
+import java.awt.event.ActionEvent;
+
 import com.print_stack_trace.voogasalad.controller.TabController;
 import com.print_stack_trace.voogasalad.model.engine.GameEngine;
+import com.print_stack_trace.voogasalad.player.Score;
 
 import javafx.scene.Group;
+import javafx.scene.control.Button;
 
 public class GamePlayer implements TabController {
 	private Group root = new Group();
 	private GameEngine gameEngine;
 	
+	private Score currentScore;
+	private Button saveGame, resumeGame, pauseGame,stopGame;
+	
+	
 	public Group initialize(GameEngine gameEngine) {
 		this.gameEngine = gameEngine;
+		
 		//TODO: Implement
+		
+		// initialize GUI Elements (method)
+		currentScore = new Score();
 		return root;
 	}
 	
@@ -21,4 +33,36 @@ public class GamePlayer implements TabController {
 	public void updateScene(){
 		
 	}
+	
+	private void updateScore(){
+		/*** 
+		 * calls gameEngine to update Score;; need to hash out out we handle scores/coins
+		 */
+		gameEngine.updateScore() //(or if we are in charge of scores.. then scores.updateScore()
+	} 
+	
+	private void pauseGame(){ //buttons with handlers
+		gameEngine.pause();
+		//if gameplayer is the gameloop --> timeline.stop();
+	}
+	
+	private void resumeGame(){ //buttons with handlers
+		gameEngine.resume();
+		//timeline.resume();
+	}
+	private void saveGame(){   //buttons with handlers
+		gameEngine.saveGame();
+	}
+	
+	private void stopGame(){
+		gameEngine.stopGame();
+	}
+	
+	private void setHandlersForGuiElements(){
+		saveGame.setOnAction(ActionEvent -> saveGame());
+		resumeGame.setOnAction(ActionEvent -> resumeGame());
+		pauseGame.setOnAction(ActionEvent -> pauseGame());
+		stopGame.setOnAction(ActionEvent -> stopGame());
+	}
+	
 }
