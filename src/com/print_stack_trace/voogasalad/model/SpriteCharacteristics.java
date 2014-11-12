348bc2a78c9a8906e24f93e0311a65c0131a876e
Zachary Podbela <podbelaapps@gmail.com>
2014-11-11 22:58:06 -0500

    Started GameEngine APi

---
+++ src/com/print_stack_trace/voogasalad/model/SpriteCharacteristics.java
@@ -0,0 +1,10 @@
+package com.print_stack_trace.voogasalad.model;
+
+import java.awt.Image;
+import java.awt.Point;
+
+public class SpriteCharacteristics {
+	Image img;
+	Point p;
+	boolean interactive;
+}
--- src/com/print_stack_trace/voogasalad/model/engine/GameEngine.java
+++ src/com/print_stack_trace/voogasalad/model/engine/GameEngine.java
@@ -1,7 +1,13 @@
 package com.print_stack_trace.voogasalad.model.engine;
 
+import java.awt.Image;
+import java.awt.Point;
+import java.io.File;
+import java.util.Map;
+
 import com.print_stack_trace.voogasalad.model.LevelModel;
 import com.print_stack_trace.voogasalad.model.RuntimeModel;
+import com.print_stack_trace.voogasalad.model.SpriteCharacteristics;
 
 public class GameEngine {
 	private LevelModel currentLevel;
@@ -17,6 +23,35 @@ public class GameEngine {
 	
 	//-------------------PUBLIC METHODS-------------------//
 	
+	//TODO: @data This should mirror public on GameData.java
+	public boolean loadGame(File gameFile) {
+		return true;
+	}
+	
+	//TODO: @data This should mirror public on GameData.java
+	public boolean saveGame(File gameFile) {
+		return false;
+	}
+	
+	//GAME AUTHORING
+	
+	Integer addObjectToLevel(SpriteCharacteristics spriteModel) {
+		//TODO: This should mirror public on LevelModel.java
+		return null;
+	}
+	
+	Integer updateObject(Integer modelID, SpriteCharacteristics spriteModel) {
+		//TODO: This should mirror public on LevelModel.java
+		return null;
+	}
+	
+	boolean deleteObject(Integer modelID) {
+		//TODO: This should mirror public on LevelModel.java
+		return false;
+	}
+	
+	//GAME PLAYER
+	
 	public RuntimeModel getStatus() {
 		RuntimeModel ret = new RuntimeModel();
 		/*TODO: Implement -- this is looks like an accessor to everyone else but
@@ -25,20 +60,21 @@ public class GameEngine {
 		return ret;
 	}
 	
+	public Map<Integer, Double> getHighScoreList() {
+		//TODO: Return high score list
+		return null;
+	}
+	
 	//-------------------ACCESSORS-------------------//
 	
 	public LevelModel getCurrentLevel() {
 		return currentLevel;
 	}
-
-	public void setCurrentLevel(LevelModel currentLevel) {
-		this.currentLevel = currentLevel;
-		loadLevel(currentLevel);
-	}
 	
 	//-------------------PRIVATE METHODS-------------------//
 	
 	private void loadLevel(LevelModel level) {
+		this.currentLevel = level;
 		//TODO: Implement -- Will be used for setLevel but also reseting level.
 	}
 }
