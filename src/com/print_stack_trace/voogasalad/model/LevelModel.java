4775c6e4a2eeb48e46ae444e11387c13ddf60dc9
jdc61 <jdc61@duke.edu>
2014-11-11 23:01:31 -0500

    LevelModel stuff

--- src/com/print_stack_trace/voogasalad/model/LevelModel.java
+++ src/com/print_stack_trace/voogasalad/model/LevelModel.java
@@ -1,5 +1,28 @@
 package com.print_stack_trace.voogasalad.model;
 
+import java.util.Map;
+
 public class LevelModel {
+	
+	Map<Integer, SpriteCharacteristics> spriteMap;
+	
+	
+	public Integer addObject (SpriteCharacteristics chars) {
+		return 0;
+	}
+	
+	public boolean deleteObject (Integer ModelID) {
+		return false;
+	}
+	
+	public boolean updateObject (Integer ModelID, SpriteCharacteristics chars) {
+		return false;
+	}
+	
+	
+	
+	
+	
+	
 
 }
