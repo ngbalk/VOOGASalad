a8e6e599df4b277f68f3dac7d34aa363deb0685e
Ethan Chang <ejc24@duke.edu>
2014-11-12 21:32:10 -0500

    goal stubs added

--- src/com/print_stack_trace/voogasalad/model/sprites/Goal.java
+++ src/com/print_stack_trace/voogasalad/model/sprites/Goal.java
@@ -1,8 +1,30 @@
 package com.print_stack_trace.voogasalad.model.sprites;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public abstract class Goal {
     
+    private double value;
+    private Collection<Sprite> mySpriteList;
+    
     public Goal() {
-        
+        mySpriteList = new ArrayList<Sprite>();
+        value = 0;
     }
+    
+    public Goal(Sprite sprite) {
+        this();
+        mySpriteList.add(sprite);
+    }
+    
+    public Goal(Collection<Sprite> spriteList){
+        this();
+        mySpriteList = spriteList;
+    }
+    
+    
+    public abstract boolean isCompleted();
+    
+    
 }
