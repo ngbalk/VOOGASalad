bc5a173dada75eabdd2965ad6fef01554924cf6b
Ashwin Kommajesula <ashwin.kommajesula@gmail.com>
2014-11-12 21:45:42 -0500

    added public methods for saving and loading high scores

--- src/com/print_stack_trace/voogasalad/model/data/GameData.java
+++ src/com/print_stack_trace/voogasalad/model/data/GameData.java
@@ -1,11 +1,12 @@
 package com.print_stack_trace.voogasalad.model.data;
 
 import java.lang.reflect.Type;
+import java.util.List;
 
 import com.print_stack_trace.voogasalad.model.LevelModel;
 
 public class GameData {
-	
+
 	/**
 	 * constructor for GameData
 	 */
@@ -22,7 +23,6 @@ public class GameData {
 	 *            - level created by the authoring environment
 	 * @param location
 	 *            - provides the location ont he hard drive to store the file
-	 * 
 	 * @param name
 	 *            - name of the file to be saved
 	 */
@@ -47,9 +47,36 @@ public class GameData {
 	 * also be used by the engine to update preferences in
 	 * 
 	 * @param name
-	 *            - ile that is being overwritten by the save
+	 *            - file that is being overwritten by the save
 	 */
 	public void saveLevel(String name) {
 
 	}
+
+	/**
+	 * adds a new high score to a text file storing the current high scores for
+	 * a given level error for now, there is a HighScores class that will be
+	 * added when merges take place
+	 * 
+	 * @param highscore
+	 *            - new high score
+	 * @param name
+	 *            - name of the level that the high score will be associated
+	 *            with
+	 */
+	public void saveHighScore(String name, HighScores highscore) {
+
+	}
+
+	/**
+	 * 
+	 * @param name
+	 *            - name of the file that the high scores are being loaded for
+	 * @return list or text representation of the high scores
+	 */
+	public List<HighScores> loadHighScores(String name) {
+		return null;
+
+	}
+
 }
