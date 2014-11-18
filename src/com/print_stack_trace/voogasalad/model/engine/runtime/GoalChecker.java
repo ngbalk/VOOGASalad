8ef399b34e4e74e29e6611d7dedb07a58175e40e
jackbaskin <jackbaskin93@gmail.com>
2014-11-18 16:53:28 -0500

    added unimplemented methods to goal checker

--- src/com/print_stack_trace/voogasalad/model/engine/runtime/GoalChecker.java
+++ src/com/print_stack_trace/voogasalad/model/engine/runtime/GoalChecker.java
@@ -1,14 +1,42 @@
 package com.print_stack_trace.voogasalad.model.engine.runtime;
 
-import com.print_stack_trace.voogasalad.model.environment.GoalEXAMPLeSUBCLASS;
 import com.print_stack_trace.voogasalad.model.environment.GoalElementVisitor;
+import com.print_stack_trace.voogasalad.model.environment.KillBoss;
+import com.print_stack_trace.voogasalad.model.environment.Points;
+import com.print_stack_trace.voogasalad.model.environment.ReachDistance;
+import com.print_stack_trace.voogasalad.model.environment.ReachObject;
+import com.print_stack_trace.voogasalad.model.environment.StayAlive;
 
 public class GoalChecker implements GoalElementVisitor {
 
 	//TODO: Change this class as expands
 	
 	@Override
-	public boolean visit(GoalEXAMPLeSUBCLASS goal) {
+	public boolean visit(KillBoss goal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Points goal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(ReachDistance goal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(ReachObject goal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(StayAlive goal) {
 		// TODO Auto-generated method stub
 		return false;
 	}
