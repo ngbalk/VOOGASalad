2fdb8e1a7b21a8c45aee7287c0a9837e37082102
Pranava Raparla <pranava.raparla@gmail.com>
2014-11-22 15:05:13 -0500

    Base CollisionFactory Class

---
+++ src/com/print_stack_trace/voogasalad/model/engine/physics/CollisionFactory.java
@@ -0,0 +1,69 @@
+package com.print_stack_trace.voogasalad.model.engine.physics;
+
+/**
+ * @author Pranava Raparla
+ * Date Created: 11/22/14
+ * Date Modified: 11/22/14
+ */
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import com.print_stack_trace.voogasalad.model.engine.physics.collisions.*;;
+
+public class CollisionFactory {
+	//TODO: verify that this path takes us to the right package
+	public static final String collisionResultPath = "com.print_stack_trace.voogasalad.model.enginge.physics.collisions";
+	
+	public enum CollisionResult {
+		ObjectOneFullDisplacement,
+		ObjectTwoFullDisplacement,
+		ObjectBothFullDisplacement,
+		ObjectBothNoDisplacement,
+		NoAction,
+		ObjectOneDisappear,
+		ObjectTwoDisappear,
+		ObjectBothDisappear,
+		GameOverLose,
+		GameOverWin
+	};
+	
+	//TODO: make sure its collision result vs. collision handler and vice-versa
+    public CollisionHandler buildGoal(CollisionResult myCollisionResult) {
+        Constructor<?> con = null;
+        CollisionHandler newCollisionResult = null;
+
+        try {
+            String objectType = myCollisionResult.name(); //TODO: explore using toString() instead of name()
+            Class<?> newCollisionResultClass = Class.forName(collisionResultPath + objectType);
+            try {
+                con = newCollisionResultClass.getConstructor(CollisionHandler.class);
+            } catch (NoSuchMethodException | SecurityException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            try {
+            	//newCollisionResult = (CollisionHandler) con.newInstance(((CollisionHandler)myCollisionResult).class);
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return newCollisionResult;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+    
+    //TODO: Determine if this method from goal characteristics is applicable...
+    private String reformatTypeString(String s) {
+        String[] words = s.split("_");
+        String reformattedString = "";
+        for(String word : words) {
+            reformattedString += word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+        }
+        return reformattedString;
+    }
+    
+}
