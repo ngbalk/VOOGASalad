2aedcab227ebd643ae55c9a85792af2d1728fc0c
Nick Balkissoon <ngb5@duke.edu>
2014-11-23 15:49:15 -0500

    Added LevelNameController

---
+++ src/com/print_stack_trace/voogasalad/controller/guiElements/LevelCharacteristicController.java
@@ -0,0 +1,28 @@
+package com.print_stack_trace.voogasalad.controller.guiElements;
+
+
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+import com.print_stack_trace.voogasalad.model.engine.authoring.LevelModel;
+
+public abstract class LevelCharacteristicController extends UserInputText{
+	protected LevelModel myLevel;
+	protected TextField myTextBox;
+	public LevelCharacteristicController(LevelModel level){
+		myLevel = level;
+		myTextBox = (TextField) myNode;
+		populateDefaultText();
+		myTextBox.textProperty().addListener(new ChangeListener<String>(){
+			@Override
+			public void changed(ObservableValue<? extends String> arg0,
+					String oldVal, String newVal) {
+					setCharacteristic(newVal);
+			}
+		});
+	}
+	abstract protected void populateDefaultText();
+	abstract protected void setCharacteristic(String newValue);
+}
--- src/com/print_stack_trace/voogasalad/model/LevelCharacteristics.java
+++ src/com/print_stack_trace/voogasalad/model/LevelCharacteristics.java
@@ -5,8 +5,15 @@ import java.io.File;
 import javafx.scene.image.Image;
 
 public class LevelCharacteristics {
+	private String name;
 	public File nextLevel = null;
 	public Image backgroundImg = null;
 	public int requiredNumberOfGoals = 0;
 	//TODO: More...
+	public String getName() {
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
 }
