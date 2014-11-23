ab8c32e28a5f4c3ff4e0f7b25b3be4d6b62b5354
Pranava Raparla <pranava.raparla@gmail.com>
2014-11-23 13:59:39 -0500

    Added default variables to SpriteCharacteristics

--- src/com/print_stack_trace/voogasalad/model/SpriteCharacteristics.java
+++ src/com/print_stack_trace/voogasalad/model/SpriteCharacteristics.java
@@ -9,6 +9,7 @@ import com.print_stack_trace.voogasalad.model.engine.authoring.GameAuthorEngine.
 
 public class SpriteCharacteristics {
 	
+	// DEFAULT VARIABLES
 	public static final Image DEFAULT_IMAGE = null;
 	public static final Point DEFAULT_POINT = new Point(0,0);
 	public static final boolean DEFAULT_INTERACTIVE = true;
@@ -16,8 +17,9 @@ public class SpriteCharacteristics {
 	public static final int DEFAULT_HEALTH = 10;
 	public static final double DEFAULT_SPEED = 10;
 	public static final int DEFAULT_VALUE = 0;
-	public static final String DEFAULT_DIRECTION_FACING = "";
+	public static final String DEFAULT_DIRECTION_FACING = "left";
 	
+	// GAME AUTHORING VARIABLES
 	public Image img;
 	public Point p;
 	public boolean interactive;
@@ -26,9 +28,11 @@ public class SpriteCharacteristics {
 	public double startingSpeed;
 	public int value;
 	public String startingDirectionFacing;
-
 	
-	
+	/**
+	 * Constructor for SpriteCharacteristics takes in an enum SpriteType t and sets values
+	 * @param t
+	 */
 	public SpriteCharacteristics(SpriteType t){
 		objectType = t;
 //		Default values 
