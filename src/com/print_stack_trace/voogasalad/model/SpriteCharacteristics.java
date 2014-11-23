0097a07d34b6d53f4734f623b7b5d63360d6022e
Pranava Raparla <pranava.raparla@gmail.com>
2014-11-23 13:55:10 -0500

    Added default variables to SpriteCharacteristics class

--- src/com/print_stack_trace/voogasalad/model/SpriteCharacteristics.java
+++ src/com/print_stack_trace/voogasalad/model/SpriteCharacteristics.java
@@ -8,6 +8,16 @@ import com.print_stack_trace.voogasalad.model.engine.authoring.GameAuthorEngine.
 //import com.print_stack_trace.voogasalad.model.engine.physics.List;
 
 public class SpriteCharacteristics {
+	
+	public static final Image DEFAULT_IMAGE = null;
+	public static final Point DEFAULT_POINT = new Point(0,0);
+	public static final boolean DEFAULT_INTERACTIVE = true;
+	public static final SpriteType DEFAULT_OBJECT_TYPE = null;
+	public static final int DEFAULT_HEALTH = 10;
+	public static final double DEFAULT_SPEED = 10;
+	public static final int DEFAULT_VALUE = 0;
+	public static final String DEFAULT_DIRECTION_FACING = "";
+	
 	public Image img;
 	public Point p;
 	public boolean interactive;
