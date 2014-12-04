7578bce068794b9762cc2f54a41a7d186db83a6d
jdc61 <jdc61@duke.edu>
2014-12-04 16:40:54 -0500

    collisions

---
+++ src/com/print_stack_trace/voogasalad/model/engine/physics/collisions/ObjectBothDisappear.java
@@ -0,0 +1,21 @@
+package com.print_stack_trace.voogasalad.model.engine.physics.collisions;
+
+import com.print_stack_trace.voogasalad.model.engine.physics.CollisionHandler;
+import com.print_stack_trace.voogasalad.model.engine.runtime.RuntimeModel;
+import com.print_stack_trace.voogasalad.model.engine.runtime.RuntimeSpriteCharacteristics;
+
+public class ObjectBothDisappear extends CollisionHandler {
+
+	@Override
+	public void applyCollisionEffects(RuntimeSpriteCharacteristics s1,
+			RuntimeSpriteCharacteristics s2, RuntimeModel currentRuntime) {
+		// TODO Auto-generated method stub
+		s1.interactive = false;
+		s1.setAlpha(0.0f);
+		s1.flagForRemoval();
+		s2.interactive = false;
+		s2.setAlpha(0.0f);
+		s2.flagForRemoval();
+	}
+
+}
