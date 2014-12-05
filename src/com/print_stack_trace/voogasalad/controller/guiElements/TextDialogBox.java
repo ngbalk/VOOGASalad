4ac5ed322a5ae8fff040ee395ec5e84619539208
Zachary Podbela <podbelaapps@gmail.com>
2014-12-05 17:32:56 -0500

    Added Code to use Generic Types DialogBox instead of casting

--- src/com/print_stack_trace/voogasalad/controller/guiElements/TextDialogBox.java
+++ src/com/print_stack_trace/voogasalad/controller/guiElements/TextDialogBox.java
@@ -5,26 +5,32 @@ import java.util.List;
 
 import javafx.scene.control.TextField;
 
-public class TextDialogBox extends AbstractDialogBox {
+public class TextDialogBox extends AbstractDialogBox<TextField> {
 
 	public TextDialogBox(TextField e) {
-		super(e);
+		super(constructorConvert(e));
 	}
 	
 	public TextDialogBox(List<TextField> e) {
 		super(e);
 	}
+	
+	private static List<TextField> constructorConvert(TextField e) {
+		List<TextField> ret = new ArrayList<TextField>();
+		ret.add(e);
+		return ret;
+	}
 
 	@Override
 	public String submit() {
-		TextField tf = this.tfList.get(0);
+		TextField tf = this.nodeList.get(0);
 		return tf.getText();
 	}
 
 	@Override
 	public List<String> submitAll() {
 		List<String> strList = new ArrayList<String>();
-		for(TextField tF : this.tfList) {
+		for(TextField tF : this.nodeList) {
 			strList.add(tF.getText());
 		}
 		return strList;
