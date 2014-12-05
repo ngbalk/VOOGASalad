3f6e70ac89a9e427431d47a7bedc74cd4ff9a8eb
dkm12@duke.edu <dkm12@duke.edu>
2014-12-05 17:49:05 -0500

    Renamed some objects to be more in-line with our naming conventions and more communicative about their purpose.

--- src/com/print_stack_trace/voogasalad/controller/guiElements/AbstractDialogBox.java
+++ src/com/print_stack_trace/voogasalad/controller/guiElements/AbstractDialogBox.java
@@ -15,28 +15,28 @@ public abstract class AbstractDialogBox<T extends Node> {
 
 	protected Button submitButton = new Button("Submit");
 	protected List<T> nodeList;
-	private Stage stage;
+	private Stage myStage;
 	
 	public AbstractDialogBox(List<T> nodeList) {
 		this.nodeList = nodeList;
-		stage = new Stage();
-		stage.setWidth(250);
-		stage.setHeight(250);
-		stage.initStyle(StageStyle.UTILITY);
+		myStage = new Stage();
+		myStage.setWidth(250);
+		myStage.setHeight(250);
+		myStage.initStyle(StageStyle.UTILITY);
 		Group root = new Group();  
 		Scene s = new Scene(root);
-		HBox box = new HBox();
-		VBox vbox = new VBox();
-		vbox.getChildren().addAll(nodeList);
-		box.getChildren().addAll(vbox, submitButton);
-		root.getChildren().add(box);
+		HBox boxForFieldsAndSubmitButton = new HBox();
+		VBox vboxForFieldsToBeFilledByUser = new VBox();
+		vboxForFieldsToBeFilledByUser.getChildren().addAll(nodeList);
+		boxForFieldsAndSubmitButton.getChildren().addAll(vboxForFieldsToBeFilledByUser, submitButton);
+		root.getChildren().add(boxForFieldsAndSubmitButton);
 		submitButton.setOnAction(e->submit());
-		stage.setScene(s);
-		stage.show();
+		myStage.setScene(s);
+		myStage.show();
 	}
 	
 	public void close(){
-		stage.close();
+		myStage.close();
 	}
 
 	public abstract String submit();
