bfd16b0694fc502e01cc731ed46e94c48d2ffee0
Zachary Podbela <podbelaapps@gmail.com>
2014-12-07 18:27:44 -0500

    Added method stubs

--- src/com/print_stack_trace/voogasalad/model/engine/runtime/camera/CameraFactory.java
+++ src/com/print_stack_trace/voogasalad/model/engine/runtime/camera/CameraFactory.java
@@ -1,11 +1,18 @@
 package com.print_stack_trace.voogasalad.model.engine.runtime.camera;
 
+import com.print_stack_trace.voogasalad.model.engine.physics.CollisionHandler;
+
 public class CameraFactory {
 
 	public enum CameraType {
 		ConstantHorizonalScroll,
 		ConstantVerticalScroll,
 		PlayerFollow
+	}
+	
+	public static CameraHandler buildCameraHandler(CameraType myCameraType){
+		return null;
+		
 	}
 
 }
