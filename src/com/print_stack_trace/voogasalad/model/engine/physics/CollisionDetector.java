54962c8115b956848596cbb69287aa42ba9815fa
Ethan Chang <ejc24@duke.edu>
2014-12-06 22:00:50 -0500

    fixed point x and y's

--- src/com/print_stack_trace/voogasalad/model/engine/physics/CollisionDetector.java
+++ src/com/print_stack_trace/voogasalad/model/engine/physics/CollisionDetector.java
@@ -21,21 +21,21 @@ public class CollisionDetector {
 	 * @return true if a collision happened, false otherwise
 	 */
 	public static boolean haveCollided(RuntimeSpriteCharacteristics spriteA, RuntimeSpriteCharacteristics spriteB) {
-		Rectangle rect1 = new Rectangle((int) spriteA.p.getX(), (int) spriteA.p.getY(), (int) spriteA.getWidth(), (int) spriteA.getHeight());
-		Rectangle rect2 = new Rectangle((int) spriteB.p.getX(), (int) spriteB.p.getY(), (int) spriteB.getWidth(), (int) spriteB.getHeight());
+		Rectangle rect1 = new Rectangle((int) spriteA.getX(), (int) spriteA.getY(), (int) spriteA.getWidth(), (int) spriteA.getHeight());
+		Rectangle rect2 = new Rectangle((int) spriteB.getX(), (int) spriteB.getY(), (int) spriteB.getWidth(), (int) spriteB.getHeight());
 		return rect1.intersects(rect2);
 	}
 
 	public static boolean haveCollidedHorizontally(RuntimeSpriteCharacteristics spriteA, RuntimeSpriteCharacteristics spriteB){
-		Rectangle rect1 = new Rectangle((int) spriteA.p.getX(), (int) spriteA.p.getY(), (int) spriteA.getWidth(), (int) spriteA.getHeight());
-		Rectangle rect2 = new Rectangle((int) spriteB.p.getX(), (int) spriteB.p.getY(), (int) spriteB.getWidth(), (int) spriteB.getHeight());
+		Rectangle rect1 = new Rectangle((int) spriteA.getX(), (int) spriteA.getY(), (int) spriteA.getWidth(), (int) spriteA.getHeight());
+		Rectangle rect2 = new Rectangle((int) spriteB.getX(), (int) spriteB.getY(), (int) spriteB.getWidth(), (int) spriteB.getHeight());
 		Rectangle rect3 = rect1.intersection(rect2);
 		return rect3.getHeight() > rect3.getWidth();
 	}
 
 	public static boolean haveCollidedVertically(RuntimeSpriteCharacteristics spriteA, RuntimeSpriteCharacteristics spriteB){
-		Rectangle rect1 = new Rectangle((int) spriteA.p.getX(), (int) spriteA.p.getY(), (int) spriteA.getWidth(), (int) spriteA.getHeight());
-		Rectangle rect2 = new Rectangle((int) spriteB.p.getX(), (int) spriteB.p.getY(), (int) spriteB.getWidth(), (int) spriteB.getHeight());
+		Rectangle rect1 = new Rectangle((int) spriteA.getX(), (int) spriteA.getY(), (int) spriteA.getWidth(), (int) spriteA.getHeight());
+		Rectangle rect2 = new Rectangle((int) spriteB.getX(), (int) spriteB.getY(), (int) spriteB.getWidth(), (int) spriteB.getHeight());
 		Rectangle rect3 = rect1.intersection(rect2);
 		return rect3.getWidth() >= rect3.getHeight();
 	}
@@ -43,8 +43,8 @@ public class CollisionDetector {
 	public static boolean haveCollidedFromTop(RuntimeSpriteCharacteristics hero,
 			RuntimeSpriteCharacteristics block){
 		if(!hero.objectType.equals(SpriteType.HERO)) return false;
-		Rectangle top = new Rectangle((int) hero.p.getX(), (int) hero.p.getY(), (int) hero.getWidth(), (int) hero.getHeight());
-		Rectangle bottom = new Rectangle((int) block.p.getX(), (int) block.p.getY(), (int) block.getWidth(), (int) block.getHeight());
+		Rectangle top = new Rectangle((int) hero.getX(), (int) hero.getY(), (int) hero.getWidth(), (int) hero.getHeight());
+		Rectangle bottom = new Rectangle((int) block.getX(), (int) block.getY(), (int) block.getWidth(), (int) block.getHeight());
 		Rectangle intersect = top.intersection(bottom); 
 		
 		hero.isCollidingHorizontally = false;
@@ -56,8 +56,8 @@ public class CollisionDetector {
 	public static boolean haveCollidedFromBottom(RuntimeSpriteCharacteristics hero,
 			RuntimeSpriteCharacteristics block){
 		if(!hero.objectType.equals(SpriteType.HERO)) return false;
-		Rectangle bottom = new Rectangle((int) hero.p.getX(), (int) hero.p.getY(), (int) hero.getWidth(), (int) hero.getHeight());
-		Rectangle top = new Rectangle((int) block.p.getX(), (int) block.p.getY(), (int) block.getWidth(), (int) block.getHeight());
+		Rectangle bottom = new Rectangle((int) hero.getX(), (int) hero.getY(), (int) hero.getWidth(), (int) hero.getHeight());
+		Rectangle top = new Rectangle((int) block.getX(), (int) block.getY(), (int) block.getWidth(), (int) block.getHeight());
 		Rectangle intersect = top.intersection(bottom);        
 		
 		hero.isCollidingHorizontally = false;
@@ -70,8 +70,8 @@ public class CollisionDetector {
 	public static boolean haveCollidedFromLeft(RuntimeSpriteCharacteristics hero,
 			RuntimeSpriteCharacteristics block){
 		if(!hero.objectType.equals(SpriteType.HERO)) return false;
-		Rectangle left = new Rectangle((int) hero.p.getX(), (int) hero.p.getY(), (int) hero.getWidth(), (int) hero.getHeight());
-		Rectangle right = new Rectangle((int) block.p.getX(), (int) block.p.getY(), (int) block.getWidth(), (int) block.getHeight());
+		Rectangle left = new Rectangle((int) hero.getX(), (int) hero.getY(), (int) hero.getWidth(), (int) hero.getHeight());
+		Rectangle right = new Rectangle((int) block.getX(), (int) block.getY(), (int) block.getWidth(), (int) block.getHeight());
 		Rectangle intersect = left.intersection(right);  
 		hero.isCollidingHorizontally = true;
 		hero.isCollidingVertically = false;
@@ -82,8 +82,8 @@ public class CollisionDetector {
 	public static boolean haveCollidedFromRight(RuntimeSpriteCharacteristics hero,
 			RuntimeSpriteCharacteristics block){
 		if(!hero.objectType.equals(SpriteType.HERO)) return false;
-		Rectangle right = new Rectangle((int) hero.p.getX(), (int) hero.p.getY(), (int) hero.getWidth(), (int) hero.getHeight());
-		Rectangle left = new Rectangle((int) block.p.getX(), (int) block.p.getY(), (int) block.getWidth(), (int) block.getHeight());
+		Rectangle right = new Rectangle((int) hero.getX(), (int) hero.getY(), (int) hero.getWidth(), (int) hero.getHeight());
+		Rectangle left = new Rectangle((int) block.getX(), (int) block.getY(), (int) block.getWidth(), (int) block.getHeight());
 		Rectangle intersect = left.intersection(right);       
 		hero.isCollidingHorizontally = true;
 		hero.isCollidingVertically = false;
