96180881b871bcd4b829f45ed8c17ed37da3bff6
dkm12@duke.edu <dkm12@duke.edu>
2014-12-07 15:58:58 -0500

    Kind of messed up Splash Screen but I can get a scroll pane in the player. Need file loading to work more to progress any further.

--- src/com/print_stack_trace/voogasalad/controller/guiElements/PlayPane.java
+++ src/com/print_stack_trace/voogasalad/controller/guiElements/PlayPane.java
@@ -1,6 +1,8 @@
 package com.print_stack_trace.voogasalad.controller.guiElements;
 
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 
 public class PlayPane extends Pane {
@@ -10,9 +12,12 @@ public class PlayPane extends Pane {
 	}
 
 	public PlayPane(Node... arg0) {
-		super(arg0);
+		//super(arg0);
 
 	}
 	
+//	public ObservableList<Node> getChildren(){
+//		return super.getChildren();
+//	}
 
 }
