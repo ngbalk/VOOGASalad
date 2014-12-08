a7075b7dc9f4b058e6fa1620396005899d18d3a6
Pranava Raparla <pranava.raparla@gmail.com>
2014-12-08 01:01:14 -0500

    Moved methods from GameWorldCharacteristics to GameWorldModel

--- src/com/print_stack_trace/voogasalad/model/GameWorldCharacteristics.java
+++ src/com/print_stack_trace/voogasalad/model/GameWorldCharacteristics.java
@@ -6,91 +6,30 @@
 
 package com.print_stack_trace.voogasalad.model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import com.print_stack_trace.voogasalad.model.engine.authoring.LevelModel;
-
 public class GameWorldCharacteristics {
     // DEFAULT VARIABLES
 	
 	
     // GAME AUTHORING VARIABLES
     private String gameTitle;
-    private Map<Integer, LevelModel> levels;
-    private List<LevelModel> levelSequence;
-    private int currentLevel;
     
     //TODO: More...
     
     //-------------------CONSTRUCTORS-------------------//
     public GameWorldCharacteristics() {
-        levels = new HashMap<>();
-        levelSequence = new LinkedList<>();
-        currentLevel = 0;
+    	
     }
     
 	//-------------------PUBLIC METHODS-------------------//
+    
+    
+    //-------------------ACCESSORS-------------------//
     public String getGameTitle() {
         return gameTitle;
     }
     public void setGameTitle(String gameTitle) {
         this.gameTitle = gameTitle;
-    }
+    }    
 
-    public int getNumberOfLevels() {
-        return levels.size();
-    }
-    
-    
-    //-------------------ACCESSORS-------------------//
-
-    public void addLevel(Integer id, LevelModel level) {
-        levels.put(id, level);
-    }
-
-    public LevelModel getNextLevel() {
-        LevelModel nextLevel = levelSequence.get(currentLevel);
-        currentLevel ++;
-        return nextLevel;
-    }
-
-    public LevelModel startNewGame() {
-        currentLevel = 0;
-        return levelSequence.get(0);
-    }
-    
-    public LevelModel getLevelModel(int id) {
-        return levels.get(id);
-    }
-    
-    public boolean verifyWorldIntegrity() {
-        return levels.size() == levelSequence.size();
-    }
-    
-    public void createGameSequence() {
-        LevelModel currentLevel = new LevelModel();
-        for(Integer id: levels.keySet()) {
-            if(levels.get(id).getLevelCharacteristics().previousLevel == null) {
-                currentLevel = levels.get(id);
-                levelSequence.add(currentLevel);
-                break;
-            }
-        }
-        for(int i =0; i< levels.size(); i++) {
-            for(int j=0; j< levels.size(); j++) {
-                if(levels.get(levels.get(j)).getLevelCharacteristics().ID == currentLevel.getLevelCharacteristics().nextLevel) {
-                    currentLevel = levels.get(j);
-                    break;
-                }
-            }
-            if(currentLevel.getLevelCharacteristics().nextLevel == null) {
-                break;
-            }
-        }
-    }
 
 }
--- src/com/print_stack_trace/voogasalad/model/engine/authoring/GameWorldModel.java
+++ src/com/print_stack_trace/voogasalad/model/engine/authoring/GameWorldModel.java
@@ -7,43 +7,58 @@
 package com.print_stack_trace.voogasalad.model.engine.authoring;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.print_stack_trace.voogasalad.model.GameWorldCharacteristics;
 
 public class GameWorldModel {
-	private List<LevelModel> levels;
-
 	private GameWorldCharacteristics gameWorldCharacteristics;
+    private Map<Integer, LevelModel> levelMap;
+    private List<LevelModel> levelSequence;
+    private Integer currentLevelIndex;
 	
 	//-------------------CONSTRUCTORS-------------------//
 	public GameWorldModel() {
-		levels = new ArrayList<LevelModel>();
+		levelMap = new HashMap<Integer, LevelModel>();
+		levelSequence = new ArrayList<LevelModel>();
 		gameWorldCharacteristics = new GameWorldCharacteristics();
 	}
 	
-	//-------------------PUBLIC METHODS-------------------//
-	public void addLevel(LevelModel newLevel) {
-		levels.add(newLevel);
-	}
-	
-	public void addLevel(LevelModel newLevel, int index) {
-		levels.add(index,newLevel);
-	}
+	//-------------------PUBLIC METHODS-------------------//	
+    public boolean verifyWorldIntegrity() {
+        return levelMap.size() == levelSequence.size();
+    }
+    
+    public void createGameSequence() {
+        LevelModel currentLevel = new LevelModel();
+        for(Integer id: levelMap.keySet()) {
+            if(levelMap.get(id).getLevelCharacteristics().previousLevel == null) {
+                currentLevel = levelMap.get(id);
+                levelSequence.add(currentLevel);
+                break;
+            }
+        }
+        for(int i =0; i< levelMap.size(); i++) {
+            for(int j=0; j< levelMap.size(); j++) {
+                if(levelMap.get(levelMap.get(j)).getLevelCharacteristics().ID == currentLevel.getLevelCharacteristics().nextLevel) {
+                    currentLevel = levelMap.get(j);
+                    break;
+                }
+            }
+            if(currentLevel.getLevelCharacteristics().nextLevel == null) {
+                break;
+            }
+        }
+    }
+    
+    public LevelModel startNewGame() {
+    	currentLevelIndex = 0;
+        return levelSequence.get(0);
+    }
 	
 	//-------------------ACCESSORS-------------------//
-	public List<LevelModel> getLevels() {
-		return levels;
-	}
-
-	public void setLevels(List<LevelModel> levels) {
-		this.levels = levels;
-	}
-	
-	public Integer getNumberOfLevels() {
-		return levels.size();
-	}
-
 	public GameWorldCharacteristics getGameWorldCharacteristics() {
 		return gameWorldCharacteristics;
 	}
@@ -52,5 +67,28 @@ public class GameWorldModel {
 			GameWorldCharacteristics gameWorldCharacteristics) {
 		this.gameWorldCharacteristics = gameWorldCharacteristics;
 	}
+
+	public LevelModel getLevelModel(int id) {
+        return levelMap.get(id);
+    }
+
+    public List<LevelModel> getLevels() {
+		return levelSequence;
+	}
+	
+	public Integer getNumberOfLevels() {
+		return levelMap.size();
+	}
+	
+    public void addLevel(Integer id, LevelModel level) {
+        levelMap.put(id, level);
+    }
+
+    public LevelModel getNextLevel() {
+        LevelModel nextLevel = levelSequence.get(currentLevelIndex);
+        currentLevelIndex++;
+        return nextLevel;
+    }
+    
 	
 }
