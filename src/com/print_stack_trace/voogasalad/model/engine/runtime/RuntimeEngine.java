d3961977605997d1fd1eeadfca56771290eac7e0
Zachary Podbela <podbelaapps@gmail.com>
2014-12-07 19:22:52 -0500

    Resolved merge conflicts

--- src/com/print_stack_trace/voogasalad/model/engine/runtime/RuntimeEngine.java
+++ src/com/print_stack_trace/voogasalad/model/engine/runtime/RuntimeEngine.java
@@ -6,6 +6,7 @@
 
 package com.print_stack_trace.voogasalad.model.engine.runtime;
 
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +17,8 @@ import javafx.scene.input.KeyEvent;
 
 import com.print_stack_trace.voogasalad.model.engine.authoring.LevelModel;
 import com.print_stack_trace.voogasalad.model.engine.physics.PhysicsEngine;
+import com.print_stack_trace.voogasalad.model.engine.runtime.camera.CameraFactory;
+import com.print_stack_trace.voogasalad.model.engine.runtime.camera.CameraHandler;
 import com.print_stack_trace.voogasalad.model.engine.runtime.keyboard.KeyApplicationChecker;
 import com.print_stack_trace.voogasalad.model.engine.runtime.keyboard.KeyApplicator;
 import com.print_stack_trace.voogasalad.model.engine.runtime.keyboard.KeyApplicatorFactory;
@@ -27,6 +30,7 @@ public class RuntimeEngine extends AbstractRuntimeEngine {
 	private RuntimeModel runtimeModel;
 	int framesPerSecond;
 	private Map<KeyResult, KeyApplicator> applicatorCache = new HashMap<KeyResult, KeyApplicator>();
+	private CameraHandler cameraHandler;
 
 	//-------------------CONSTRUCTORS-------------------//
 
@@ -34,10 +38,11 @@ public class RuntimeEngine extends AbstractRuntimeEngine {
 	 * Takes in a LevelModel and sets private variables
 	 * @param level
 	 */
-	public RuntimeEngine(LevelModel currentLevel) {
+	public RuntimeEngine(LevelModel currentLevel, Dimension viewport) {
 		super(currentLevel);
-		runtimeModel = new RuntimeModel(currentLevel);
+		runtimeModel = new RuntimeModel(currentLevel, viewport);
 		physicsEngine = currentLevel.getPhysicsEngine();
+		cameraHandler = CameraFactory.buildCameraHandler(currentLevel.getLevelCharacteristics().cameraType);
 	}
 
 	//-------------------PUBLIC METHODS-------------------//
@@ -48,6 +53,7 @@ public class RuntimeEngine extends AbstractRuntimeEngine {
 	 * 2. Apply unexpired key events.
 	 * 3. Use GoalChecker to check goals
 	 * 4. See if game is over or not
+	 * 5. Move camera
 	 * @param currentLevel
 	 */
 	public void update() {
@@ -69,6 +75,8 @@ public class RuntimeEngine extends AbstractRuntimeEngine {
 		}
 
 		updateSpritePositions();
+		
+		cameraHandler.updateCamera(runtimeModel);
 	}
 
 	public void setFramesPerSecond(int framesPerSecond) {
